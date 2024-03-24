820073013714101f31359860be0c255f7a10189d
EdisonP <133362311+edions@users.noreply.github.com>
2024-03-25 00:15:17 +0800

    Fix error when image is empty if update

--- api/product/update.php
+++ api/product/update.php
@@ -5,25 +5,37 @@ $id = $_POST['id'];
 $name = $_POST['name'];
 $category = $_POST['cat'];
 $quantity = $_POST['stock'];
-$file = $_FILES['img'];
 
 try {
-    $filename = time() . '_' . basename($_FILES["img"]["name"]);
-    $target_file = '../../uploads/' . $filename;
-    move_uploaded_file($_FILES["img"]["tmp_name"], $target_file);
+    
+    if(isset($_FILES['img'])) {
 
-    $target_file = str_replace("../../", "", $target_file);
+        $filename = time() . '_' . basename($_FILES["img"]["name"]);
+        $target_file = '../../uploads/' . $filename;
+        move_uploaded_file($_FILES["img"]["tmp_name"], $target_file);
+        $target_file = str_replace("../../", "", $target_file);
 
-    $query = "UPDATE product
-              SET product_name = :name, product_category = :category, product_quantity = :quantity, product_image = :picture
-              WHERE product_id = :id";
-    $statement = $connection->prepare($query);
-    $statement->bindParam(':id', $id);
-    $statement->bindParam(':name', $name);
-    $statement->bindParam(':category', $category);
-    $statement->bindParam(':quantity', $quantity);
-    $statement->bindParam(':picture', $target_file);
-    $statement->execute();
+        $query = "UPDATE product
+                SET product_name = :name, product_category = :category, product_quantity = :quantity, product_image = :picture
+                WHERE product_id = :id";
+        $statement = $connection->prepare($query);
+        $statement->bindParam(':id', $id);
+        $statement->bindParam(':name', $name);
+        $statement->bindParam(':category', $category);
+        $statement->bindParam(':quantity', $quantity);
+        $statement->bindParam(':picture', $target_file);
+        $statement->execute();
+    } else {
+        $query = "UPDATE product
+                SET product_name = :name, product_category = :category, product_quantity = :quantity
+                WHERE product_id = :id";
+        $statement = $connection->prepare($query);
+        $statement->bindParam(':id', $id);
+        $statement->bindParam(':name', $name);
+        $statement->bindParam(':category', $category);
+        $statement->bindParam(':quantity', $quantity);
+        $statement->execute();
+    }
 
     echo json_encode(["res" => "success"]);
 } catch(PDOException $th) {
